b923b24080cfab0c5a02382bdad32484aa242aca
chuan <1516998446@qq.com>
2024-08-09 02:47:28 +0800

    WelcomeAndLogin一个稳定版本，修复了hover无响应的bug

--- FrontEnd/src/pages/WelcomeAndLogin.jsx
+++ FrontEnd/src/pages/WelcomeAndLogin.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 
-function WelcomeAndLogin() {
-    return (
+//TODO:完成欢迎以及登录界面
+//TODO:完成前后端链接
 
-        <div
-            style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                zIndex: -1,
-                maxWidth: "100%",
-                maxHeight: "100%",
-                backgroundImage: "url('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80)",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-            }}
-            className="bg-image,"
-        >
-            <div className="relative bg-gray-900 bg-opacity-75 h-screen" >
+export default function WelcomeAndLogin() {
+    return (
+        <div style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            // zIndex: -1,
+            maxWidth: "100%",
+            maxHeight: "100%",
+            // backgroundImage: "url('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80)",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+        }} className="relative">
+            <img
+                src="https://images.pexels.com/photos/3747463/pexels-photo-3747463.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                className="absolute inset-0 object-cover w-full h-full"
+                alt=""
+            />
+            <div style={{ zIndex: 1000 }} className="relative bg-gray-900 bg-opacity-75 h-screen " >
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="flex flex-col items-center justify-between xl:flex-row">
                         <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
                             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
                                 欢迎使用<br className="hidden md:block" /><br className="hidden md:block" />
-                                <span className="text-teal-accent-400">兴趣圈</span>
+                                <span className="text-teal-400">兴趣圈</span>
                             </h2>
                             <p className="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
                                 <br className="hidden md:block" />探索未知 · 分享热爱 · 表达自我
@@ -70,7 +74,13 @@ function WelcomeAndLogin() {
                                         />
                                     </div>
                                     <div className="mt-4 mb-2 sm:mb-4">
-                                        <button className="flex items-center justify-center h-12 px-6 w-full bg-blue-600 mt-8 rounded font-semibold text-lg text-blue-100 hover:bg-blue-700" onClick={() => { }}>登录</button>
+                                        {/* <button className="flex items-center justify-center h-12 px-6 w-full bg-blue-600 mt-8 rounded font-semibold text-lg text-blue-100 hover:bg-blue-700" onClick={() => { }}>登录</button> */}
+                                        <button
+                                            type="submit"
+                                            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none"
+                                        >
+                                            Subscribe
+                                        </button>
                                         <a className="text-blue-400 hover:text-blue-500" href="#">没有账号？立即注册</a>
                                     </div>
                                 </form>
@@ -79,14 +89,10 @@ function WelcomeAndLogin() {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
 
     );
 };
-//TODO:完成欢迎以及登录界面
-
-
-export default WelcomeAndLogin;
 
 function BackgroundImage() {
     return (

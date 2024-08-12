252f6e9a14296ed98d28a3238e57b4d0925d6528
chuan <1516998446@qq.com>
2024-08-13 03:03:47 +0800

    tmp

--- FrontEnd/src/pages/Circles.jsx
+++ FrontEnd/src/pages/Circles.jsx
@@ -44,8 +44,6 @@ export default function Circles() {
     )
 }
 
-
-
 function Main({ circles }) {
 
     return (
@@ -73,7 +71,6 @@ function Main({ circles }) {
                                         circles.map((circle) => {
                                             return (
                                                 <>
-
                                                     <tr>
                                                         <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
                                                             <span className="ml-3 font-bold text-white"> {circle.name} </span></th>
@@ -81,21 +78,12 @@ function Main({ circles }) {
                                                         </td>
                                                         <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                                                             <div className="flex">
-
-
-
                                                                 {circle.lovers.map((lover) => {
-
                                                                     return (
                                                                         <img src={lover.userImgURL} className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" />
+                                                                        // {/** -ml-4 */ }
                                                                     )
                                                                 })}
-
-                                                                {/* <img src="https://demos.creative-tim.com/notus-js/assets/img/team-1-800x800.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow" />
-                                                <img src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4" />
-                                                <img src="https://demos.creative-tim.com/notus-js/assets/img/team-3-800x800.jpg" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4" />
-                                                <img src="https://demos.creative-tim.com/notus-js/assets/img/team-4-470x470.png" alt="..." className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4" /> */}
-
                                                             </div>
                                                         </td>
                                                         <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
@@ -117,9 +105,6 @@ function Main({ circles }) {
                                             )
                                         })
                                     }
-
-
-
                                 </tbody>
                             </table>
                         </div>
